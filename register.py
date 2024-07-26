a6632b821a78a723986577c32565713d192b20aa
henrique-paiva-uece <121185490+henrique-paiva-uece@users.noreply.github.com>
2024-07-26 10:38:42 -0300

    Add register file

---
+++ register.py
@@ -0,0 +1,28 @@
+from google.cloud import bigquery
+from datetime import datetime
+#from bigquery import get_client
+
+def on_bigquery(hostname, ip, datetime, is_detected):
+
+#    datetime = datetime.now()
+#    hostname = "desktop"
+#    ip = "192.168.0.0"
+#    is_detected = False
+
+    project_id = 'malware-detection-422423' # Fill with project ID
+    service_account = 'bq-user@malware-detection-422423.iam.gserviceaccount.com>
+    dataset = "detection_records" # Fill with dataset name
+    table = "records" # Fill with table name
+
+    # JSON key provided by Google
+    json_key = 'malware-detection-422423-bde15ff25756.json'
+
+    client = bigquery.Client(json_key_file=json_key)
+
+    rows =  [
+        {'hostname': hostname, 'ip': ip, "is_malware_detected": is_detected,  "timestamp": datetime} ]
+
+    # Insert data into table.
+    inserted = client.push_rows(dataset, table, rows)
+    
+    return
