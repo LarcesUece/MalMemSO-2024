0d341af7966d6f9ba8a304a52f6e8ddd4fc8a5c0
Jessica Patricio <jessicacacau@gmail.com>
2024-12-11 22:13:40 -0300

    add documentation

--- device/src/modules/dump_extractor.py
+++ device/src/modules/dump_extractor.py
@@ -6,19 +6,38 @@ from subprocess import Popen, PIPE, CalledProcessError
 from utils import BIN_PATH, RAW_PATH, create_dir, delete_file
 
 
-def extract_dump(tool=None, arch=None):
-    """Run the memory dump tool."""
+def extract_dump(tool, arch):
+    """Extracts the memory dump file using the specified tool and
+    architecture.
+
+    Validates the tool and architecture provided, gets the path to the
+    tool executable, creates the output path for the memory dump file,
+    and executes the tool command. If an error occurs during memory
+    dump extraction, the output file is deleted.
+
+    Args:
+        tool (str): Tool used to extract the memory dump file.
+        arch (str): Architecture of the target system.
+
+    Returns:
+        str: Path to the memory dump file.
+
+    Raises:
+        ValueError: If the tool or architecture is not provided or
+        invalid.
+        PermissionError: If the raw output directory cannot be created.
+        Exception: If an error occurs during memory dump extraction.
+    """
+
+    info("Running memory dump extraction.")
 
     formatted_arch = _validate_extract_dump_args(tool, arch)
-
-    info(f"Extracting memory dump using {tool.capitalize()} tool.")
-
     tool_path = join(BIN_PATH, f"{tool}_{formatted_arch}.exe")
     output_path = _get_output_path(tool, formatted_arch)
     command = _get_tool_command(tool, tool_path, output_path)
-
-    info(f"Running command: {command}.")
     extraction_error = False
+
+    info(f"Extracting memory dump using {tool.capitalize()} tool.")
 
     try:
         process = Popen(
@@ -48,16 +67,121 @@ def extract_dump(tool=None, arch=None):
         delete_file(output_path)
         raise Exception(error_message)
 
-    info(f"Output message: {stdout}")
+    info(f"Output message: {stdout}.")
     info(f"Memory dump file saved at {output_path}.")
 
     return output_path
 
 
-def _get_tool_command(tool, tool_path, output_path):
-    """Get the command to execute the memory dump tool."""
+def _validate_extract_dump_args(tool, arch):
+    """Validates the arguments for the extract_dump function.
 
-    info(f"Getting command for {tool.capitalize()} tool")
+    Checks if the tool and architecture are provided and valid. The
+    architecture is formatted as "x86" or "x64" based on the provided
+    architecture. Returns the formatted architecture.
+
+    Args:
+        tool (str): Tool used to extract the memory dump file.
+        arch (str): Architecture of the target system.
+
+    Returns:
+        str: Formatted architecture.
+
+    Raises:
+        ValueError: If the tool or architecture is not provided or
+        invalid.
+    """
+
+    info("Validating tool and arch provided for memory dump extraction.")
+
+    if not tool or not arch:
+        error_message = "Tool and architecture must be provided."
+        error(error_message)
+        raise ValueError(error_message)
+
+    if not isinstance(tool, str) or not isinstance(arch, str):
+        error_message = "Tool and architecture must be strings."
+        error(error_message)
+        raise ValueError(error_message)
+
+    info("Tool is valid.")
+
+    if arch == "32bit":
+        formatted_arch = "x86"
+    elif arch == "64bit":
+        formatted_arch = "x64"
+    else:
+        error_message = f"Architecture not supported: {arch}."
+        error(error_message)
+        raise ValueError(error_message)
+
+    info("Arch is valid.")
+
+    return formatted_arch
+
+
+def _get_output_path(tool, arch):
+    """Gets the output path for the memory dump file.
+
+    Creates the raw output directory if it does not exist, gets the
+    next available number for the memory dump file, and returns the
+    output path. The output path is formatted as
+    "dump_<tool>_<arch>_<number>.raw". The number is incremented by 1
+    for each memory dump file existing in the directory.
+
+    Args:
+        tool (str): Tool used to extract the memory dump file.
+        arch (str): Architecture of the target system.
+
+    Returns:
+        str: Path to the memory dump file.
+
+    Raises:
+        PermissionError: If the raw output directory cannot be created.
+    """
+
+    if not exists(RAW_PATH):
+        try:
+            create_dir(RAW_PATH)
+            info("Raw output directory created.")
+        except:
+            error_message = "Failed to create raw output directory."
+            error(error_message)
+            raise PermissionError(error_message)
+
+    info("Getting output path for memory dump file.")
+
+    files = listdir(RAW_PATH)
+    files = [f for f in files if f.startswith(f"dump_{tool}_{arch}_")]
+    numbers = [int(f.split("_")[-1].split(".")[0]) for f in files]
+    number = max(numbers) + 1 if numbers else 1
+
+    return join(RAW_PATH, f"dump_{tool}_{arch}_{number}.raw")
+
+
+def _get_tool_command(tool, tool_path, output_path):
+    """Gets the command to execute the memory dump tool.
+
+    Checks if the tool is supported and returns the command to execute
+    the tool.
+
+    Args:
+        tool (str): Tool used to extract the memory dump file.
+        tool_path (str): Path to the tool executable.
+        output_path (str): Path to the memory dump file.
+
+    Returns:
+        list: Command to execute the memory dump tool.
+
+    Raises:
+        ValueError: If the tool is not supported.
+
+    Supported tools:
+        WinPmem
+        DumpIt for Windows
+    """
+
+    info(f"Getting command for {tool.capitalize()} tool.")
 
     COMMANDS = {
         "winpmem": [tool_path, output_path],
@@ -72,63 +196,23 @@ def _get_tool_command(tool, tool_path, output_path):
     return COMMANDS[tool]
 
 
-def _get_output_path(tool, arch):
-    """Get the output path for the memory dump file.
-
-    The format of the output path is dump_<tool>_<arch>_<number>.raw.
-    - tool: Tool used to extract the memory dump file.
-    - arch: Architecture of the target system.
-    - number: Next available number for the memory dump file.
-    """
-
-    if not exists(RAW_PATH):
-        try:
-            create_dir(RAW_PATH)
-        except:
-            error_message = "Failed to create raw output directory."
-            error(error_message)
-            raise PermissionError(error_message)
-
-    info("Getting output path for memory dump file")
-
-    files = listdir(RAW_PATH)
-    files = [f for f in files if f.startswith(f"dump_{tool}_{arch}_")]
-    numbers = [int(f.split("_")[-1].split(".")[0]) for f in files]
-    number = max(numbers) + 1 if numbers else 1
-
-    return join(RAW_PATH, f"dump_{tool}_{arch}_{number}.raw")
-
-
 def _is_dump_empty(filepath):
-    """Check if the dump file is empty."""
+    """Checks if the dump file is empty.
+
+    Checks if the memory dump file exists and has a size greater than
+    0. Returns True if the file does not exist or is empty, False
+    otherwise.
+
+    Args:
+        filepath (str): Path to the memory dump file.
+
+    Returns:
+        bool: True if the file is empty or does not exist, False
+        otherwise.
+    """
 
     if exists(filepath):
         file_size = stat(filepath).st_size
         return not file_size
 
     return True
-
-
-def _validate_extract_dump_args(tool, arch):
-    """Validate the arguments for the extract_dump function."""
-
-    if not tool or not arch:
-        error_message = "Tool and architecture must be provided."
-        error(error_message)
-        raise ValueError(error_message)
-
-    if not isinstance(tool, str) or not isinstance(arch, str):
-        error_message = "Tool and architecture must be strings."
-        error(error_message)
-        raise ValueError(error_message)
-
-    if arch == "32bit":
-        formatted_arch = "x86"
-    elif arch == "64bit":
-        formatted_arch = "x64"
-    else:
-        error_message = f"Architecture not supported: {arch}."
-        error(error_message)
-        raise ValueError(error_message)
-
-    return formatted_arch
--- device/src/modules/file_compressor.py
+++ device/src/modules/file_compressor.py
@@ -1,25 +1,46 @@
-from zipfile import ZipFile, ZIP_DEFLATED
-from time import time
 from logging import error, info
 from os.path import basename, join, exists
+from time import time
+from zipfile import ZipFile, ZIP_DEFLATED
 
 from utils import ZIP_PATH
 
 
 def compress_file(filepath):
-    """Compress a file into a ZIP archive."""
+    """Compresses a file into a ZIP archive.
+
+    Compresses the file at the given path into a ZIP archive and saves
+    it in the zip output directory. The compressed file is named after
+    the original file with a .zip extension.
+
+    Args:
+        filepath (str): Path to the file to compress.
+
+    Returns:
+        str: Path to the compressed file.
+
+    Raises:
+        FileNotFoundError: If the file does not exist.
+        Exception: If an error occurs while compressing the file.
+    """
+
+    info("Running dump file compression.")
 
     if not exists(filepath):
         error_message = f"File does not exist at {filepath}."
         error(error_message)
         raise FileNotFoundError(error_message)
 
+    info("Getting output path for compressed file.")
+
     file_name = basename(filepath)
     zip_filepath = join(ZIP_PATH, file_name + ".zip")
 
+    info("Compressing file into ZIP archive.")
+
     try:
         start = time()
-        with ZipFile(join(ZIP_PATH, file_name + ".zip"), "w", ZIP_DEFLATED) as zipf:
+        with ZipFile(zip_filepath, "w", ZIP_DEFLATED) as zipf:
             zipf.write(filepath, arcname=file_name)
         end = time()
         duration = end - start
@@ -29,4 +50,6 @@ def compress_file(filepath):
         raise Exception(error_message)
 
     info(f"File compressed successfully in {duration:.2f} seconds.")
+    info(f"Compressed file saved at {zip_filepath}.")
+
     return zip_filepath
